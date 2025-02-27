a25ae5768538521f259b39305cf98a06ffe8191b
sekibuuun <akaunto.no.001@gmail.com>
2025-02-27 11:22:00 +0900

    feat: getDelayClass()の追加

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
 }
+
+export const getDelayClass = (index: number) => {
+	const delays = [
+		'delay-0',
+		'delay-100',
+		'delay-200',
+		'delay-300',
+		'delay-400',
+		'delay-500',
+		'delay-700',
+		'delay-1000',
+	]
+	return index < delays.length ? delays[index] : 'delay-1000'
+}
