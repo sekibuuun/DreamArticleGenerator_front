e85e258249f3e747653110e4d9566b657de5d9da
sekibuuun <akaunto.no.001@gmail.com>
2025-02-27 18:30:36 +0900

    feat: SakuraBackgroundを共通コンポーネントに

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { SakuraBackground } from '@/components/SakuraBackground'
 import { Router } from '@/routes/router'
 
 export default function App() {
 	return (
 		<div>
+			<SakuraBackground />
 			<Router />
 		</div>
 	)
--- src/components/SakuraBackground.tsx
+++ src/components/SakuraBackground.tsx
@@ -4,6 +4,9 @@ export function SakuraBackground() {
 	const { canvasRef } = useSakuraBackground()
 
 	return (
-		<canvas ref={canvasRef} className="fixed inset-0 pointer-events-none z-0" />
+		<canvas
+			ref={canvasRef}
+			className="fixed inset-0 pointer-events-none z-30"
+		/>
 	)
 }
