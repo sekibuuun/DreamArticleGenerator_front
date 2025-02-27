421a21c97e03d71e8ca46c1f55ff901a10b6f8bc
Eve04lim <k05y21@icloud.com>
2025-02-27 13:13:18 +0900

    /assets/tsx作成

---
+++ src/assets/SocialIcons.tsx
@@ -0,0 +1,9 @@
+import type React from 'react'
+
+export const DividerIcon: React.FC = () => (
+  <img 
+    src="/src/assets/DividerIcon.svg" 
+    alt="Divider"
+    className="w-6 h-6 text-pink-300" 
+  />
+)
