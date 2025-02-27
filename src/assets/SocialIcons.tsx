90199eb457b5ee88472b66c6a4d84ad52237d9fe
sekibuuun <akaunto.no.001@gmail.com>
2025-02-27 16:14:37 +0900

    del

--- src/assets/SocialIcons.tsx
+++
@@ -1,9 +0,0 @@
-import type React from 'react'
-
-export const DividerIcon: React.FC = () => (
-	<img
-		src="/src/assets/DividerIcon.svg"
-		alt="Divider"
-		className="w-6 h-6 text-pink-300"
-	/>
-)
