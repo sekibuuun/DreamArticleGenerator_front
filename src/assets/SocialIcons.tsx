033bbd4278fd4aee83400712c9fb38e853edceba
Eve04lim <k05y21@icloud.com>
2025-02-27 13:18:45 +0900

    ソーシャルアイコンのコンポーネントを更新して、SVGアセットを参照

--- src/assets/SocialIcons.tsx
+++ src/assets/SocialIcons.tsx
@@ -1,9 +1,9 @@
 import type React from 'react'
 
 export const DividerIcon: React.FC = () => (
-  <img 
-    src="/src/assets/DividerIcon.svg" 
-    alt="Divider"
-    className="w-6 h-6 text-pink-300" 
-  />
+	<img
+		src="/src/assets/DividerIcon.svg"
+		alt="Divider"
+		className="w-6 h-6 text-pink-300"
+	/>
 )
